a3dafb546dc5b8d77545e249a3df9cc7d30ccccc
Rintu Kutum <rintukutum@gmail.com>
2017-08-05 16:15:05 +0530

    homework : solution 1 added

--- class-2/class-2.R
+++ class-2/class-2.R
@@ -1,4 +1,56 @@
 #-----
+# class-1
+#-----
+dnaORrna <- function(sequnce){
+	#-----
+	# upper
+	sequnce <- toupper(sequnce);
+	#------
+	# ATGC
+	# AUGC
+	#------
+	# sequnce <- 'ATGC'
+	# 1. strsplit
+	# 2. is there any T --> DNA
+	# 3. is there any U --> RNA
+	nuc <- strsplit(sequnce, split='')[[1]]
+	if(length(nuc) == 0){
+		stop('No sequence available')
+	}
+	#-----
+	#conflictSeq <- intersect(
+	#		c('T','U'),
+	#		nuc)
+	#-----
+	# setdiff
+	conflictSeq <- setdiff(
+			c('T','U'),
+			nuc)
+	#if(length(conflictSeq) == 0){
+	if(length(conflictSeq) == 2){
+		output <- 'DNA or RNA'
+	}else{
+		# check for U
+		rna <- length(which(nuc == "U"))
+		if(rna >= 1){
+			rna.nuc <- c('A', 'U', 'G', 'C')
+			if(length(setdiff(nuc, rna.nuc)) == 0){
+				output <- 'RNA'
+			}else{
+				output <- 'Given sequence is not correct'
+			}
+		}else{
+			dna.nuc <- c('A', 'T', 'G', 'C')
+			if(length(setdiff(nuc, dna.nuc)) == 0){
+				output <- 'DNA'
+			}else{
+				output <- 'Given sequence is not correct'
+			}
+		}
+	}
+	return(output)
+}
+#-----
 # class-2 materials
 #-----
 # create a matrix
